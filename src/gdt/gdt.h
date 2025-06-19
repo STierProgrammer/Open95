f6f798ceb4af7e1a83fc7376369cc1546136659d
stierprogrammer <sabasajaia40@gmail.com>
2025-06-19 19:54:33 +0400

    experimented a bit with a framebuffer

--- src/gdt/gdt.h
+++ src/gdt/gdt.h
@@ -1,6 +1,8 @@
 #ifndef OPEN95_GDT_H
 #define OPEN95_GDT_H
 
+#include <stdint.h>
+
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 // Set the base revision to 3, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
-
 __attribute__((used, section(".limine_requests")))
 static volatile LIMINE_BASE_REVISION(3);
 
@@ -14,7 +13,6 @@ static volatile LIMINE_BASE_REVISION(3);
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent, _and_ they should be accessed at least
 // once or marked as used with the "used" attribute as done here.
-
 __attribute__((used, section(".limine_requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
@@ -23,7 +21,6 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
 
 // Finally, define the start and end markers for the Limine requests.
 // These can also be moved anywhere, to any .c file, as seen fit.
-
 __attribute__((used, section(".limine_requests_start")))
 static volatile LIMINE_REQUESTS_START_MARKER;
 
@@ -37,8 +34,8 @@ static volatile LIMINE_REQUESTS_END_MARKER;
 // They CAN be moved to a different .c file.
 
 void *memcpy(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = (uint8_t *)dest;
-    const uint8_t *psrc = (const uint8_t *)src;
+    uint8_t *pdest = (uint8_t*)dest;
+    const uint8_t *psrc = (const uint8_t*)src;
 
     for (size_t i = 0; i < n; i++) {
         pdest[i] = psrc[i];
@@ -48,7 +45,7 @@ void *memcpy(void *dest, const void *src, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-    uint8_t *p = (uint8_t *)s;
+    uint8_t *p = (uint8_t*)s;
 
     for (size_t i = 0; i < n; i++) {
         p[i] = (uint8_t)c;
@@ -58,8 +55,8 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = (uint8_t *)dest;
-    const uint8_t *psrc = (const uint8_t *)src;
+    uint8_t *pdest = (uint8_t*)dest;
+    const uint8_t *psrc = (const uint8_t*)src;
 
     if (src > dest) {
         for (size_t i = 0; i < n; i++) {
@@ -75,8 +72,8 @@ void *memmove(void *dest, const void *src, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *p1 = (const uint8_t *)s1;
-    const uint8_t *p2 = (const uint8_t *)s2;
+    const uint8_t *p1 = (const uint8_t*)s1;
+    const uint8_t *p2 = (const uint8_t*)s2;
 
     for (size_t i = 0; i < n; i++) {
         if (p1[i] != p2[i]) {
@@ -115,7 +112,7 @@ void kmain(void) {
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
     for (size_t i = 0; i < 100; i++) {
         volatile uint32_t *fb_ptr = framebuffer->address;
-        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xff0000;
     }
 
     // We're done, just hang...
