83b1dcb3fc567644f3640385810155e734419188
stierprogrammer <sabasajaia40@gmail.com>
2025-07-12 21:47:31 +0400

    Added exceptions

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -1,5 +1,6 @@
 #include "idt.h"
 #include "isr_handlers.h"
+#include <stdint.h>
 
 static struct IDT idt __attribute__((aligned(4096)));
 static struct IDTR idtr __attribute__((aligned(16)));
@@ -21,9 +22,28 @@ void createDescriptor(uint8_t index, uint8_t type_and_attributes, void (*handler
 }
 
 void initIDT(void) {
-  createDescriptor(0x00, 0x8E, isr_divide_error);
-  createDescriptor(0x06, 0x8E, isr_invalid_opcode);
-  createDescriptor(0x08, 0x8E, isr_double_fault);
+  const uint8_t type_and_attributes = 0x8E;
+  createDescriptor(0x00, type_and_attributes, isr_divide_error);
+  createDescriptor(0x01, type_and_attributes, isr_debug_exception);
+  createDescriptor(0x02, type_and_attributes, isr_nmi_interrupt);
+  createDescriptor(0x03, type_and_attributes, isr_breakpoint);
+  createDescriptor(0x04, type_and_attributes, isr_overflow);
+  createDescriptor(0x05, type_and_attributes, isr_bound_range_exceeded);
+  createDescriptor(0x06, type_and_attributes, isr_invalid_opcode);
+  createDescriptor(0x07, type_and_attributes, isr_device_not_avaliable);
+  createDescriptor(0x08, type_and_attributes, isr_double_fault);
+  createDescriptor(0x09, type_and_attributes, isr_coprocessor_segment_overrun);
+  createDescriptor(0x0A, type_and_attributes, isr_invalid_tss);
+  createDescriptor(0x0B, type_and_attributes, isr_segment_not_present);
+  createDescriptor(0x0C, type_and_attributes, isr_stack_segment_fault);
+  createDescriptor(0x0D, type_and_attributes, isr_general_protection_fault);
+  createDescriptor(0x0E, type_and_attributes, isr_page_fault);
+  createDescriptor(0x10, type_and_attributes, isr_floating_point_error);
+  createDescriptor(0x11, type_and_attributes, isr_alignment_check);
+  createDescriptor(0x12, type_and_attributes, isr_machine_check);
+  createDescriptor(0x13, type_and_attributes, isr_simd_floating_point_exception);
+  createDescriptor(0x14, type_and_attributes,isr_virtualization_exception);
+  createDescriptor(0x15, type_and_attributes, isr_control_protection_exception);
 
   idtr.size = sizeof(idt) - 1;
   idtr.offset = (uint64_t)idt.descriptors;
--- src/idt/isr_handlers.h
+++ src/idt/isr_handlers.h
@@ -5,8 +5,26 @@
 #include "idt.h"
 
 extern void isr_divide_error();
+extern void isr_debug_exception();
+extern void isr_nmi_interrupt();
+extern void isr_breakpoint();
+extern void isr_overflow();
+extern void isr_bound_range_exceeded();
 extern void isr_invalid_opcode();
+extern void isr_device_not_avaliable();
 extern void isr_double_fault();
+extern void isr_coprocessor_segment_overrun();
+extern void isr_invalid_tss();
+extern void isr_segment_not_present();
+extern void isr_stack_segment_fault();
+extern void isr_general_protection_fault();
+extern void isr_page_fault();
+extern void isr_floating_point_error();
+extern void isr_alignment_check();
+extern void isr_machine_check();
+extern void isr_simd_floating_point_exception();
+extern void isr_virtualization_exception();
+extern void isr_control_protection_exception();
 
 extern void isr_exception();
 void isr_exception_handler(ExceptionFrame* eframe);
